ae9cd8347f1339ceb06f53ac23464bf53c89d421
叶继伟 <729721503@qq.com>
2022-02-21 03:37:29 +0800

    feat: 添加vue.config.js配置

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  outputDir: './build',
+  // publicPath: './',
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://152.136.185.210:4000',
+        pathRewrite: {
+          '^/api': ''
+        },
+        changeOrigin: true
+      }
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: '@/components'
+      }
+    }
+  }
+}
