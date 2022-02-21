7e7bb424ed39e2ebfde054303ca268128b2b88d9
叶继伟 <729721503@qq.com>
2022-02-22 03:14:24 +0800

    feat: 完成登录页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,36 @@
 <template>
-  <router-view />
+  <div class="app">
+    <el-config-provider :locale="zhCn">
+      <router-view></router-view>
+    </el-config-provider>
+  </div>
 </template>
 
-<style lang="less"></style>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ElConfigProvider
+  },
+  props: {
+    name: {
+      type: String
+    }
+  },
+  setup() {
+    return {
+      zhCn
+    }
+  }
+})
+</script>
+<style lang="less">
+.app {
+  height: 100%;
+}
+</style>
