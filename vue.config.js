b4acaafe6d3d41aa18bdf571a8bc51cbe6a51964
yjw <yejw6@asiainfo.com>
2022-02-21 11:07:37 +0800

    feat: 集成axios 配置 封装

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,3 @@
-// const path = require('path')
-const AutoImport = require('unplugin-auto-import/webpack')
-const Components = require('unplugin-vue-components/webpack')
-const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
-
 module.exports = {
   outputDir: './build',
   // publicPath: './',
@@ -23,13 +18,5 @@ module.exports = {
         components: '@/components'
       }
     }
-  },
-  plugins: [
-    AutoImport({
-      resolvers: [ElementPlusResolver()]
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()]
-    })
-  ]
+  }
 }
