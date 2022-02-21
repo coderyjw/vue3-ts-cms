8f066917920acab2202cf7088b05fd2be3097bda
叶继伟 <729721503@qq.com>
2022-02-22 02:48:53 +0800

    feat: 添加normalize.css 搭建login main模块

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import { globalRegister } from './global'
+import '@/assets/css/index.less'
+import 'normalize.css'
+
+import App from './App.vue'
 
 import router from './router'
 import store from './store'
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,17 +4,15 @@ import Home from '../views/Home.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    component: () => import('@/views/login/index.vue')
+  },
+  {
+    path: '/main',
+    component: () => import('@/views/main/index.vue')
   }
 ]
 
