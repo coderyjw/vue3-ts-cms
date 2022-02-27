a53f53e2c9d97eb68f338da3b3d0f6a586b44621
叶继伟 <729721503@qq.com>
2022-02-28 00:20:50 +0800

    feat: 添加logo和title

--- src/components/nav-menu/src/nav-menu.vue
+++ src/components/nav-menu/src/nav-menu.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>navMenu</div>
+  <div class="nav-menu">
+    <div class="logo">
+      <img class="img" src="~@/assets/img/logo.svg" alt="logo" />
+      <span class="title">Vue3+TS</span>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -11,5 +16,29 @@ export default defineComponent({
   }
 })
 </script>
+<style scoped lang="less">
+.nav-menu {
+  height: 100%;
+  background-color: #001529;
 
-<style scoped></style>
+  .logo {
+    display: flex;
+    height: 28px;
+    padding: 12px 10px 8px 10px;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+
+    .img {
+      height: 100%;
+      margin: 0 10px;
+    }
+
+    .title {
+      font-size: 16px;
+      font-weight: 700;
+      color: white;
+    }
+  }
+}
+</style>
