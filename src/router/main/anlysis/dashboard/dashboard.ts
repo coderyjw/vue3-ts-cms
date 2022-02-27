449af5e5c3518558629ffaae247f951ed1be1053
叶继伟 <729721503@qq.com>
2022-02-28 01:49:25 +0800

    feat: 通过coderwhy生成各模块文件以及对应路由
    使用了coderwhy工具 使用方法 1.注册npm i coderwhy -g 2.生成文件 coderwhy addpage3 [name] -d [path]

---
+++ src/router/main/anlysis/dashboard/dashboard.ts
@@ -0,0 +1,7 @@
+const dashboard = () => import('@/views/main/anlysis/dashboard/dashboard.vue')
+export default {
+  path: '/main/anlysis/dashboard',
+  name: 'dashboard',
+  component: dashboard,
+  children: []
+}
---
+++ src/router/main/anlysis/overview/overview.ts
@@ -0,0 +1,7 @@
+const overview = () => import('@/views/main/anlysis/overview/overview.vue')
+export default {
+  path: '/main/anlysis/overview',
+  name: 'overview',
+  component: overview,
+  children: []
+}
