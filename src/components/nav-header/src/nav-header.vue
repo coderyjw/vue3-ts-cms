d9a209904a45ae9ca8e2905f491a93e46ab022fe
yjw <yejw6@asiainfo.com>
2022-02-28 14:45:23 +0800

    feat: nav-header添加用户信息（user-info）

--- src/components/nav-header/src/nav-header.vue
+++ src/components/nav-header/src/nav-header.vue
@@ -4,18 +4,24 @@
       <fold v-show="isFold" />
       <expand v-show="!isFold" />
     </el-icon>
+    <div class="content">
+      <div>面包屑</div>
+      <user-info />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import { Fold, Expand } from '@element-plus/icons-vue'
+import UserInfo from './user-info.vue'
 
 export default defineComponent({
   emits: ['foldChange'],
   components: {
     Fold,
-    Expand
+    Expand,
+    UserInfo
   },
   setup(props, { emit }) {
     const isFold = ref(false)
@@ -34,9 +40,20 @@ export default defineComponent({
 
 <style scoped lang="less">
 .nav-header {
+  display: flex;
+  width: 100%;
+
   .fold-menu {
     font-size: 30px;
     cursor: pointer;
+  }
+
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    padding: 0 20px;
   }
 }
 </style>
